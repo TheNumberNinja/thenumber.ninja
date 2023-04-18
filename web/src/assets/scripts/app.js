80984ec59f6889ced9cb7181e434a03c2740ef20
Stuart Chapman <StuBez@users.noreply.github.com>
2023-04-18 14:14:46 +0100

    Manage Slideout.js using NPM
    Easier than manually managing files in the `libs` directory.
    
    Also fixed bug that would prevent the hamburger from appearing if the
    page width was too large when the page was first loaded but it became
    narrow enough bu resizing.

--- web/src/assets/scripts/app.js
+++ web/src/assets/scripts/app.js
@@ -1,30 +1,26 @@
-// hamburger toggler
 const hamburgerToggle = document.getElementById('hamburger')
+const opener = document.getElementsByClassName('header--menu_opener').item(0)
+const closer = document.getElementsByClassName('mobile_sidebar--closer').item(0)
+
 hamburgerToggle.addEventListener('click', function(e) {
   e.preventDefault()
   hamburgerToggle.classList.toggle('is-active')
 })
 
-// slideout menu
-if (window.innerWidth <= 992) {
-  const slideout = new Slideout({
-    'panel': document.getElementById('main'),
-    'menu': document.getElementById('mobile_sidebar'),
-    'padding': 320,
-    'tolerance': 70
-  })
+const slideout = new Slideout({
+  'panel': document.getElementById('main'),
+  'menu': document.getElementById('mobile_sidebar'),
+  'padding': 320,
+  'tolerance': 70
+})
 
-  const opener = document.getElementsByClassName('header--menu_opener').item(0)
-  const closer = document.getElementsByClassName('mobile_sidebar--closer').item(0)
+opener.addEventListener('click', function(e) {
+  e.preventDefault()
+  slideout.toggle()
+})
 
-  opener.addEventListener('click', function(e) {
-    slideout.toggle()
-    e.preventDefault()
-  })
-
-  closer.addEventListener('click', function(e) {
-    e.preventDefault()
-    hamburgerToggle.classList.remove('is-active')
-    slideout.close()
-  })
-}
+closer.addEventListener('click', function(e) {
+  e.preventDefault()
+  hamburgerToggle.classList.remove('is-active')
+  slideout.close()
+})
