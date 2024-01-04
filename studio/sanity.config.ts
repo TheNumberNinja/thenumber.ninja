96fdfa4ab539fc17cda33f89514fc5ab13830a65
Stuart Chapman <StuBez@users.noreply.github.com>
2024-01-04 12:30:57 +0000

    Filter ordered services
    Only show services that will be shown in the services list in the
    orderable list.

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -40,27 +40,11 @@ export default defineConfig({
             // Minimum required configuration
             orderableDocumentListDeskItem({
               type: 'service',
-              title: 'Services',
+              title: 'Services page order',
+              filter: `"websiteServicesPage" in destinations`,
               S,
               context
             }),
-          //
-          //   // Optional configuration
-          //   orderableDocumentListDeskItem({
-          //     type: 'service',
-          //     title: 'Services',
-          //     // icon: Paint,
-          //     // Required if using multiple lists of the same 'type'
-          //     // id: 'orderable-en-projects',
-          //     // See notes on adding a `filter` below
-          //     // filter: `__i18n_lang == $lang`,
-          //     // params: {
-          //     //   lang: 'en_US'
-          //     // },
-          //     // pass from the structure callback params above
-          //     S,
-          //     context
-          //   }),
           ])
       }
     }),
