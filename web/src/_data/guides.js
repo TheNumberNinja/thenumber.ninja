a59e0eeab6004f314f647c8974126cdc005cca28
Stuart Chapman <StuBez@users.noreply.github.com>
2024-01-12 13:41:08 +0000

    Display Guidejars on website [CU-86bwv0rth]
    https://app.clickup.com/t/86bwv0rth
    
    Works the same as forms: The details are manually added to `guides.js`
    and the list and individual pages are automatically created.

---
+++ web/src/_data/guides.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    slug: 'setting-a-tracking-category-for-an-invoice-in-dext',
+    title: 'Setting a tracking category for an invoice in Dext',
+    id: '3afaa192-2531-423c-bd9c-4f45b7a7b3a4',
+    category: 'Dext',
+  },
+  {
+    slug: 'accept-our-request-to-get-access-to-your-store',
+    title: 'Accept our request to get access to your store',
+    id: '77c8b470-56b4-4f03-8b49-b2e2ec717a5e',
+    category: 'Shopify',
+  },
+  {
+    slug: 'generate-a-collaborator-request-code-so-we-can-request-access-to-your-store',
+    title: 'Generate a collaborator request code so we can request access to your store',
+    id: '8aeaab35-6c0c-4770-b863-fef232088167',
+    category: 'Shopify',
+  },
+  {
+    slug: 'create-a-xero-bill-with-a-tracking-category',
+    title: 'Create a Xero bill with a tracking category',
+    id: 'd87f4218-92d9-4d84-aada-2985aea23eb7',
+    category: 'Xero',
+  },
+  {
+    slug: 'create-a-xero-sales-invoice-with-a-tracking-category',
+    title: 'Create a Xero sales invoice with a tracking category',
+    id: '64d563a7-6076-4f6b-a7e6-cad81c736d1c',
+    category: 'Xero',
+  },
+  {
+    slug: 'invite-us-to-your-xero-account',
+    title: 'Invite us to your Xero account',
+    id: '1c09101c-7020-4626-bc78-6d4bfe9340d5',
+    category: 'Xero',
+  },
+]
