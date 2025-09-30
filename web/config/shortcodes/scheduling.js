5258b9999e3efa45fbd52de3cb9c1a9266ef9ac0
Stuart Chapman <StuBez@users.noreply.github.com>
2025-09-30 23:22:23 +0100

    Use TFH bookings domain

--- web/config/shortcodes/scheduling.js
+++ web/config/shortcodes/scheduling.js
@@ -1,6 +1,6 @@
 const trafft = uuid => {
-  return `<iframe 
-    src="https://bookings.thenumber.ninja/booking-embedded?t=s&uuid=${uuid}" 
+  return `<iframe
+    src="https://bookings.thefinancialhelm.co.uk/booking-embedded?t=s&uuid=${uuid}"
     style="border: none; width: 100%; max-width:1200px; min-height: 655px; margin: 0 auto; display: block;">
   </iframe>`;
 };
